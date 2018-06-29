b598f7ed5ec4f27192baa8b224967aa3046be5d9
Wazons <wazom@live.cn>
2018-06-29 22:15:47 +0800

    异步调用加快速度

--- js/analysis.js
+++ js/analysis.js
@@ -2,6 +2,7 @@ var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
     hm.src = "https://hm.baidu.com/hm.js?e8e4720752ae4172d75aa09cdfa2c1a9";
+    hm.async = true;
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
@@ -9,6 +10,7 @@ var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
     hm.src = "https://www.googletagmanager.com/gtag/js?id=UA-111485362-1";
+    hm.async = true;
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
